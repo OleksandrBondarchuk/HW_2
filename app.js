5d78a114a2faece11b63e4eecf48df2e71419dc0
OleksandrBondarchuk <102091804+OleksandrBondarchuk@users.noreply.github.com>
2022-10-10 18:03:27 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+let number1 = prompt('Please add number #1', 2);
+let number2 = prompt('Please add number #2', 2);
+number1 = parseInt(number1);
+number2 = parseInt(number2);
+let sum = number1 + number2;
+alert('Your result ' + number1 + ' + ' + number2 + ' = '  + sum);
+let subtraction = number1 - number2;
+alert('Your result ' + number1 + ' - ' + number2 + ' = '  + subtraction);
+let multiplication = number1 * number2;
+alert('Your result ' + number1 + ' * ' + number2 + ' = '  + multiplication);
+let division = number1 / number2;
+alert('Your result ' + number1 + ' / ' + number2 + ' = '  + division);
+
+
+
+
+
+
+
+
+
+
+
